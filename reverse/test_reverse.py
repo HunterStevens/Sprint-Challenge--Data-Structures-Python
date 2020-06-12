488a9a2a13a8fffcc191ed25b3cf8aef3596dea4
Hunter Stevens <hunterstevens1996@gmail.com>
2020-06-12 13:49:27 -0600

    getting the reverse list to work

--- reverse/test_reverse.py
+++ reverse/test_reverse.py
@@ -10,7 +10,6 @@ class LinkedListTests(unittest.TestCase):
     self.assertEqual(self.list.head.value, 1)
     self.list.add_to_head(2)
     self.assertEqual(self.list.head.value, 2)
-
   def test_contains(self):
     self.list.add_to_head(1)
     self.list.add_to_head(2)
@@ -18,7 +17,8 @@ class LinkedListTests(unittest.TestCase):
     self.assertTrue(self.list.contains(2))
     self.assertTrue(self.list.contains(10))
     self.assertFalse(self.list.contains(1000))
-
+  '''
+  '''
   def test_empty_reverse(self):
     self.list.reverse_list(self.list.head, None)
     self.assertEqual(self.list.head, None)
@@ -27,7 +27,6 @@ class LinkedListTests(unittest.TestCase):
     self.list.add_to_head(1)
     self.list.reverse_list(self.list.head, None)
     self.assertEqual(self.list.head.value, 1)
-
   def test_longer_reverse(self):
     self.list.add_to_head(1)
     self.list.add_to_head(2)
@@ -39,6 +38,7 @@ class LinkedListTests(unittest.TestCase):
     self.assertEqual(self.list.head.value, 1)
     self.assertEqual(self.list.head.get_next().value, 2)
     self.assertEqual(self.list.head.get_next().get_next().value, 3)
-    
+  '''
+  '''
 if __name__ == '__main__':
   unittest.main()
